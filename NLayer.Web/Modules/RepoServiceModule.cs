079a685902fc1a05d717cb6071d5ded61dac3e5b
Ali Siyar Arslan <siyararslan891@gmail.com>
2024-04-07 21:55:06 +0300

    46. ProductController-2

---
+++ NLayer.Web/Modules/RepoServiceModule.cs
@@ -0,0 +1,52 @@
+﻿using Autofac;
+using NLayer.Core.Repositories;
+using NLayer.Core.Services;
+using NLayer.Core.UnitOfWorks;
+using NLayer.Repository;
+using NLayer.Repository.Repositories;
+using NLayer.Repository.UnitOfWorks;
+using NLayer.Service.Mapping;
+using NLayer.Service.Services;
+using System.Reflection;
+using Module = Autofac.Module;
+
+namespace NLayer.Web.Modules
+{
+    public class RepoServiceModule : Module
+    {
+        protected override void Load(ContainerBuilder builder)
+        {
+
+            builder.RegisterGeneric(typeof(GenericRepository<>))
+                .As(typeof(IGenericRepository<>))
+                .InstancePerLifetimeScope();
+
+            builder.RegisterGeneric(typeof(Service<>))
+                .As(typeof(IService<>))
+                .InstancePerLifetimeScope();
+
+            builder.RegisterType<UnitOfWork>().As<IUnitOfWork>();
+
+            var apiAssembly = Assembly.GetExecutingAssembly();
+
+            //repostiroy katmanındaki herhangi bir sınıf
+            var repoAssembly = Assembly.GetAssembly(typeof(AppDbContext));
+
+
+            //service katmanındaki herhangi bir sınıf
+            var serviceAssembly = Assembly.GetAssembly(typeof(MapProfile));
+
+            builder.RegisterAssemblyTypes(apiAssembly, repoAssembly, serviceAssembly)
+                .Where(t => t.Name.EndsWith("Repository"))
+                .AsImplementedInterfaces()
+                .InstancePerLifetimeScope();
+
+            builder.RegisterAssemblyTypes(apiAssembly, repoAssembly, serviceAssembly)
+                .Where(t => t.Name.EndsWith("Service"))
+                .AsImplementedInterfaces()
+                .InstancePerLifetimeScope();
+
+            
+        }
+    }
+}
