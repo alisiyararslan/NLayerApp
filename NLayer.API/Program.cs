6633f521cbeef9c6d12f7736a0b959d59caa5393
Ali Siyar Arslan <siyararslan891@gmail.com>
2024-04-07 10:15:12 +0300

    37. FluentValidation Library-2

--- NLayer.API/Program.cs
+++ NLayer.API/Program.cs
@@ -10,14 +10,19 @@ using NLayer.Service.Services;
 using NLayer.Service.Validations;
 using System.Reflection;
 using FluentValidation.AspNetCore;
+using NLayer.API.Filters;
+using Microsoft.AspNetCore.Mvc;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-builder.Services.AddControllers().AddFluentValidation(x => x.RegisterValidatorsFromAssemblyContaining<ProductDtoValidator>());
+builder.Services.AddControllers(options => options.Filters.Add(new ValidateFilterAttribute())).AddFluentValidation(x => x.RegisterValidatorsFromAssemblyContaining<ProductDtoValidator>());
 
-
+builder.Services.Configure<ApiBehaviorOptions>(options =>
+{
+    options.SuppressModelStateInvalidFilter = true;
+});
 
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
